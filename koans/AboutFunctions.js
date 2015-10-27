722068ab0e6082ff15f37e40782718164a721d67
Kartik Vempati <kvempati11@gmai.com>
2015-10-26 22:21:46 -0700

    Correct function to string method comments.

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -107,15 +107,18 @@ describe("About Functions", function() {
       return a * b;
     };
     var answer = multiply.toString()  // This is a placeholder answer
-    expect(multiply.toString()).toBe(answer);
+    expect(multiply.toString()).toBe("function(a, b) {
+    //  // An internal comment
+    //  return a * b;
+    // };");
     // This is what I believe to be the actual answer for this test.
     // I checked it with the the console in Chrome as well, but for some reason
     // I am not able to pass on the tester. 
     //
-    // function(a, b) {
+    // "function(a, b) {
     //  // An internal comment
     //  return a * b;
-    // };
+    // };"
 
   });    
 });
